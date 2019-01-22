1c88a4666a2b202a3e9704d480f487818c5a5066
Chase Hamby <c.chase.hamby@gmail.com>
2019-01-21 19:05:51 -0600

    added google image button and centered

--- src/components/pages/Auth/auth.js
+++ src/components/pages/Auth/auth.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import authRequests from '../../../helpers/data/authRequests';
 import './auth.scss';
+import googleButton from '../../../images/login-google.png';
 
 class Auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate().then(() => {
+      this.props.history.push('/home');
+    }).catch(err => console.error('error in auth', err));
+  }
+
   render() {
     return (
       <div className='Auth'>
-        <button className='btn btn-danger'>
-          Authenticate
+        <button className='btn btn-danger' onClick={this.authenticateUser}>
+          <img src={googleButton} alt="google login button"/>
         </button>
       </div>
     );
