d3e21da88cbafb61f6bf60a8f3f4f9c538abc333
Chase Hamby <c.chase.hamby@gmail.com>
2019-01-21 20:01:01 -0600

    homepage working with fully functioning profile button and continue button

--- src/components/pages/Home/home.js
+++ src/components/pages/Home/home.js
@@ -4,17 +4,22 @@ import profileButton from '../../../images/createChild.png';
 import continueButton from '../../../images/continue.png';
 
 class Home extends React.Component {
+  changeView = (e) => {
+    const view = e.currentTarget.id;
+    this.props.history.push(`/${view}`);
+  }
+
   render() {
     return (
       <div className='Home mx-auto'>
         <div className='homePage'>
           <div className="profileBtn">
-          <button className='btn btn-light'>
+          <button className='btn btn-light' id='childProfile' onClick={this.changeView}>
             <img src={profileButton} alt="create child profile button"/>
           </button>
           </div>
           <div className="continueBtn">
-          <button className='btn btn-light'>
+          <button className='btn btn-light' id='resources' onClick={this.changeView}>
             <img src={continueButton} alt="continue to site button"/>
           </button>
           </div>
