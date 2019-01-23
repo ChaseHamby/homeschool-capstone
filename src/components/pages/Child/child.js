68e0e18bea462809bda592391c12757b8cc49ab6
Chase Hamby <c.chase.hamby@gmail.com>
2019-01-23 17:18:57 -0600

    delete working

--- src/components/pages/Child/child.js
+++ src/components/pages/Child/child.js
@@ -1,10 +1,29 @@
 import React from 'react';
-// import authRequests from '../../../helpers/data/authRequests';
+import authRequests from '../../../helpers/data/authRequests';
+import childRequests from '../../../helpers/data/childRequests';
 import './child.scss';
 
 class Child extends React.Component {
   render() {
-    // const myUid = authRequests.getCurrentUid();
+    const myUid = authRequests.getCurrentUid();
+    const deleteChild = (e) => {
+      childRequests.deleteChild(this.props.id)
+        .then(() => {
+          this.props.updateChildren();
+        })
+        .catch(err => console.error('error with single delete', err));
+    };
+
+    const showDeleteButton = () => {
+      if (this.props.uid === myUid) {
+        return (
+          <div>
+          <button className="trash" onClick={deleteChild}><i className="far fa-trash-alt fa-2x"></i></button>
+        </div>
+        );
+      }
+    };
+
     return (
       <div className="container py-3">
       <div className="card">
@@ -15,6 +34,7 @@ class Child extends React.Component {
                 <p className="card-text">{this.props.email}</p>
                 <p className="card-text"><b>Subject: {this.props.age}</b></p>
                 <p className="card-text"><b>Grade: {this.props.grade}</b></p>
+                <div className="d-flex justify-content-center">{showDeleteButton()}</div>
               </div>
             </div>
           </div>
